77c39d191d07ffb3346dfbcc5e659ed32103e999
budhathokidai <budhathokirabi07@gmail.com>
2025-12-28 08:44:18 +0545

    data types

--- F_data_types/a_check_datatypes.java
+++ F_data_types/a_check_datatypes.java
@@ -1,15 +1,15 @@
 package F_data_types;
 
 public class a_check_datatypes {
-    public static void main(String[] args) {
+    /* public static void main(String[] args) {
 
         int a = 56;
         Float f = 44.44f;
         Double d = 45.44;
         String name = "Rabi";
-        System.out.println(a.getClass().getName());
+        System.out.println(.getName());
         System.out.println(f.getClass().getName());
         System.out.println(d.getClass().getName());
         System.out.println(name.getClass().getName());
-    }
+    } */
 }
