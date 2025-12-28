64aeecef48a447713aa54737293bb6ecfc456491
budhathokidai <budhathokirabi07@gmail.com>
2025-12-28 08:43:37 +0545

    inner

--- L_inner_class/b_nested_class.java
+++ L_inner_class/b_nested_class.java
@@ -1,23 +1,23 @@
 package L_inner_class;
 
-class outer{
-    int a=100;
+class Outer {
+    int a = 100;
 
-class inner{
-    String name="Hari";
+    class Inner {
+        String name = "Hari";
 
-class inner_2{
-    int b=234567890;
-}
-}
+        class Inner_2 {
+            int b = 234567890;
+        }
+    }
 }
 public class b_nested_class {
     public static void main(String[] args) {
-        outer o=new outer();
-        outer .inner i=o.new inner();
-        outer.inner.inner_2 r= i.new inner_2();
-        System.out.println(o.a+ "" +i.name+""+r.b);
+        Outer o = new Outer();
+        Outer.Inner i = o.new Inner();
+        Outer.Inner.Inner_2 r = i.new Inner_2();
+        System.out.println(o.a + " " + i.name + " " + r.b);
     }
-}
+} 
 
 
